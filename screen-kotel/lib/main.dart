f0b20d92b6c339871ffca7595a1ba99f3bc57e69
Tiago <tiagoalmeidasp@hotmail.com>
2025-07-17 14:40:53 -0300

    tela kotel finalizada

--- screen-kotel/lib/main.dart
+++ screen-kotel/lib/main.dart
@@ -3,6 +3,21 @@ import 'package:flutter/material.dart';
 void main() => runApp(MeuApp());
 
 class MeuApp extends StatelessWidget {
+  callAction() {
+    // Ação para o botão de ligar
+    print('Ligar pressionado');
+  }
+
+  mapAction() {
+    // Ação para o botão de mapa
+    print('Mapa pressionado');
+  }
+
+  shareAction() {
+    // Ação para o botão de compartilhar
+    print('Compartilhar pressionado');
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -21,18 +36,104 @@ class MeuApp extends StatelessWidget {
                   Expanded(
                     child: Column(
                       crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Text('Kotel'),
-                        Text('Jerusalém, Israel'),
+                      children: const [
+                        Text(
+                          'Kotel',
+                          style: TextStyle(
+                            fontSize: 18,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.black,
+                          ),
+                        ),
+                        Text(
+                          'Jerusalém, Israel',
+                          style: TextStyle(
+                            fontSize: 16,
+                            color: Colors.grey,
+                          ),
+                        )
                       ],
                     ),
                   ),
                   Row(
-                    children: [Icon(Icons.star, color: Colors.blue), Text('5')],
+                    children: const [
+                      Icon(
+                        Icons.star,
+                        color: Colors.blue,
+                      ),
+                      Text('9,786'),
+                    ],
                   )
                 ],
               ),
-            )
+            ),
+            Container(
+              padding: const EdgeInsets.all(20),
+              child: Row(
+                children: [
+                  Botao(
+                    icon: Icons.call,
+                    text: 'Ligar',
+                    onPress: callAction,
+                  ),
+                  const SizedBox(width: 10), // Espaçamento entre os botões
+                  Botao(
+                      icon: Icons.location_on,
+                      text: 'Mapa',
+                      onPress: mapAction),
+                  const SizedBox(width: 10), // Espaçamento entre os botões
+                  Botao(
+                    icon: Icons.share,
+                    text: 'Compartilhar',
+                    onPress: shareAction,
+                  ),
+                ],
+              ),
+            ),
+            Container(
+              padding: const EdgeInsets.all(20),
+              child: const Text(
+                'O Muro das Lamentações, também conhecido como Kotel, é um local sagrado para o judaísmo, localizado em Jerusalém. É o único remanescente do Segundo Templo de Jerusalém e é um destino de peregrinação para judeus de todo o mundo.',
+                style: TextStyle(fontSize: 16, color: Colors.black54),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+// ignore: must_be_immutable
+class Botao extends StatelessWidget {
+  Botao({
+    this.onPress,
+    required this.icon,
+    required this.text,
+  });
+
+  Function()? onPress;
+  IconData icon;
+  String text;
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      // Inserir um botão dentro de um Expanded
+      child: ElevatedButton(
+        onPressed: onPress,
+        style: ElevatedButton.styleFrom(backgroundColor: Colors.white),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Icon(
+              icon,
+              color: Colors.blue,
+            ),
+            Text(
+              text,
+              style: const TextStyle(color: Colors.black, fontSize: 16),
+            ),
           ],
         ),
       ),
